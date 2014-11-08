093876257f12c913b5423c78dfc5172347bd4ec0
Andrey Yanov <andrey.cplusplus@gmail.com>
2014-11-08 12:17:53 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import socket
+from threading import Thread
+
+HOST = ''
+PORT = 10015
+
+
+def client_proc(conn, addr):
+    print('Accepted connection, client addr: ', addr)
+
+    conn.send('What is your name ?'.encode('utf-8'))
+    name = conn.recv(1024).decode('utf-8')
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        msg = data.decode('utf-8')
+        if msg == '<quit>':
+            break
+        print("{} ({}): ".format(name, addr[0]), msg)
+
+    print("{} ({}) disconnected".format(name, addr[0]))
+    conn.close()
+
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen(1)
+    print('Started listening on port ', PORT)
+
+    while True:
+        conn, addr = s.accept()
+
+        client_thread = Thread(target=client_proc, kwargs={'conn': conn, 'addr': addr})
+        client_thread.start()
+
+if __name__ == '__main__':
+    main()
